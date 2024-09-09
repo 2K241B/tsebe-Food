670d2ace1587dfa2eac56b1d8db43583e190f971
24LP8296 24LP8296 <24LP8296@G301-CC0203-2.local>
2024-09-09 18:03:08 +0800

    2

---
+++ food-delivery/FE/src/app/login/page.js
@@ -0,0 +1,17 @@
+import { Footer } from '@/components/Footer'
+import { Navbar } from '@/components/Navbar'
+// import { SignIn } from '@/components/SignIn'
+// import { SignUp } from '@/components/SignUp'
+import React from 'react'
+
+const Signin = () => {
+  return (
+        <div>
+            <Navbar/>
+        {/* <SignIn/> */}
+        <Footer/>
+        </div>
+  )
+}
+
+export default Signin
